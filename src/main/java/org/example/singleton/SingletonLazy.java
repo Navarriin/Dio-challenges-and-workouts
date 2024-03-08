c6961157f88015911aecd53cc70875e72bb8cd1c
Gabriel <gabrielnavarro.vip@gmail.com>
2024-03-08 15:55:53 -0300

    praticando o metodo Singleton

---
+++ src/main/java/org/example/singleton/SingletonLazy.java
@@ -0,0 +1,17 @@
+package org.example.singleton;
+import java.util.Objects;
+
+public class SingletonLazy {
+    private static SingletonLazy instance;
+
+    private SingletonLazy(){
+        super();
+    }
+
+    public static SingletonLazy getInstance(){
+        if(Objects.isNull(instance)) {
+            instance = new SingletonLazy();
+        }
+        return instance;
+    }
+}
