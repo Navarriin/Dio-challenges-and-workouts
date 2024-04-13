266541dca6ef14ee2c56ea09d491dfbc6f6b75a9
Gabriel <gabrielnavarro.vip@gmail.com>
2024-04-13 17:49:06 -0300

    Refactor: API migration from java to kotlin

---
+++ Avengers/src/main/kotlin/com/navarro/Avengers/AvengersApplication.kt
@@ -0,0 +1,11 @@
+package com.navarro.Avengers
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AvengersApplication
+
+fun main(args: Array<String>) {
+	runApplication<AvengersApplication>(*args)
+}
---
+++ Avengers/src/test/kotlin/com/navarro/Avengers/AvengersApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.navarro.Avengers
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AvengersApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
